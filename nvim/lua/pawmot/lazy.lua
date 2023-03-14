655f625468aaf8ae821e6ae728ec4a73bc528f5e
Pawel Motyl <pawmot@gmail.com>
2023-03-14 14:03:51 +0100

    [NVIM] Replace gitgutter with gitsigns

--- nvim/lua/pawmot/lazy.lua
+++ nvim/lua/pawmot/lazy.lua
@@ -11,15 +11,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- TODO: review the signs
--- TODO: the signs don't work when in after - find out why
-vim.g.gitgutter_sign_added = '✚'
-vim.g.gitgutter_sign_modified = '∼'
-vim.g.gitgutter_sign_removed = '-'
-vim.g.gitgutter_sign_removed_first_line = '⊼'
-vim.g.gitgutter_sign_modified_removed = '≃'
-vim.g.airline_powerline_fonts = 1
-
 require('lazy').setup({
   {
     'catppuccin/nvim',
@@ -63,7 +54,51 @@ require('lazy').setup({
   'airblade/vim-rooter',
   'editorconfig/editorconfig-vim',
   'tpope/vim-fugitive',
-  'airblade/vim-gitgutter',
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup {
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, { expr = true })
+
+          map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, { expr = true })
+
+          -- Actions
+          map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+          map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+          map('n', '<leader>hS', gs.stage_buffer)
+          map('n', '<leader>hu', gs.undo_stage_hunk)
+          map('n', '<leader>hR', gs.reset_buffer)
+          map('n', '<leader>hp', gs.preview_hunk)
+          map('n', '<leader>hb', function() gs.blame_line { full = true } end)
+          map('n', '<leader>tb', gs.toggle_current_line_blame)
+          map('n', '<leader>hd', gs.diffthis)
+          map('n', '<leader>hD', function() gs.diffthis('~') end)
+          map('n', '<leader>td', gs.toggle_deleted)
+
+          -- Text object
+          map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        end
+      }
+    end
+  },
   'lukas-reineke/indent-blankline.nvim',
   {
     'phaazon/hop.nvim',
